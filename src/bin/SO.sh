c3b973d19f7e0bb7a47f8a14731ce1a3aa23f328
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-08 15:25:59 -0300

    Tirado arquivos

--- src/bin/SO.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 3 ]; then
-  echo "Uso: $0 username hostname password"
-  exit 1
-fi
-
-/usr/local/bin/sshpass -p $3 ssh -q -o StrictHostKeyChecking=no $1@$2 uname -s 2> /dev/null
