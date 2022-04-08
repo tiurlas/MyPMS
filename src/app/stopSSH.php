d369f1fee06988ca7a04bceb98693a9e2878791d
Gabriel Lisboa <spgabriel08@hotmail.com>
2022-04-08 15:20:40 -0300

    Alterado branch

--- src/app/stopSSH.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $pNumber = $_POST['p'];
-    $cmd = "sudo /app/stopTerm.sh {$pNumber}";
-    $rNumber = exec($cmd);
-    $response = array("success" => true, "value" => "{$rNumber}");
-    echo json_encode($response);
-?>
